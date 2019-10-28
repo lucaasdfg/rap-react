b942488708374867b521e33d63ff814ad00f133d
Lucasdfg <cervantes.lucas99@gmail.com>
2019-10-28 15:40:55 -0600

    added some ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 import Timer from "./Components/Timer"
-import Word from './Components/Word';
-
 function App() {
   return (
     <div className="App">
       <Timer/>
-      <Word/>
     </div>
   );
 }
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,10 +1,30 @@
 import React, {Component} from "react"
+
 class Timer extends Component{
     state = {
         minutes: 5,
-        seconds: 0
+        seconds: 0,
+        randomItem: ""
     }
+    myArray = [
+        "esplendor",
+        "diciendo",
+        "impredecible",
+        "problema",
+        "terreno",
+        "instante",
+    ];
+    randomItemGenerator = () => (
+        this.myArray[Math.floor(Math.random()*this.myArray.length)]
+    )
+  
+  
+    
     componentDidMount(){
+        this.interval = setInterval(() => {
+            this.setState({randomItem:this.randomItemGenerator()})
+        }, 10000)
+        
         this.myInterval = setInterval(()=> {
             const {seconds, minutes} = this.state
 
@@ -23,16 +43,22 @@ class Timer extends Component{
                     }))
                 }
             }
+            
         }, 1000)
     }
     componentWillUnmount(){
         clearInterval(this.myInterval)
+        this.interval = setInterval(() => {
+            this.setState({randomItem:this.randomItemGenerator()})
+        }, 5000)
     }
     render(){
         const {minutes, seconds} = this.state
+
         return(
             <div>
-                {minutes === 0 && seconds === 0 ? <h1>finish</h1> : <h2>Time remaining: {minutes}:{seconds}</h2>}
+                {minutes === 0 && seconds === 0 ? <h1>finish</h1> : <h1>Time remaining: {minutes}:{seconds}</h1>}
+                <h3>{this.state.randomItem}</h3>
             </div>
         )
     }
